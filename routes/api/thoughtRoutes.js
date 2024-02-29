58e6ed621df40e2a504a3d7ca1e60a9ecb90afcb
LE Palmer <lpalmer@ucsd.edu>
2024-02-29 11:12:04 -0800

    set up thought routes and user routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+
+const {
+  getThoughts,
+  getOneThought,
+  createThought,
+  deleteThought,
+  updateThought,
+  createReaction,
+  deleteReaction,
+} = require(`../../controllers/thoughtController`);
+
+router.route("/").get(getThoughts).post(createThought);
+router
+  .route("/:thoughtId")
+  .get(getOneThought)
+  .put(updateThought)
+  .delete(deleteThought);
+router.route('/:thoughtId/reactions').post(createReaction)
+
+router
+  .route("/:thoughtId/reactions/:reactionId")
+  .delete(deleteReaction);
+
+module.exports = router;
